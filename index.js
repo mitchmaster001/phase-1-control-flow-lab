1683a716ff4f3758116ee2053cf3e8f2d5ed1149
Charles Njoroge <charles.maina@student.moringaschool.com>
2023-03-19 04:25:01 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  
+  if (someNumber <= 400){
+     
+    return "This one is for me!";
+  }
+  else if (someNumber > 400 && someNumber <= 800){
+    return "I will gladly take two thousand.";
+  }
+  else if (someNumber > 800 && someNumber <= 1200){
+    return "I will gladly take three thousand.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  if (city==="Nairobi"){
+    return "Nairobi is my favorite city.";
+  }
+  else{
+    return "i will visit";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  if (tip==="generous"){
+    return "Thank you so much.";
+
+  }else (tip === "not as generous")  
+    return "Thank you.";
+
+  }elseif
+    return "Bye.";
+
+ 
+
+  
